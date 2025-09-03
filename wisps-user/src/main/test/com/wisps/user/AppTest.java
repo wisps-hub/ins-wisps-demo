9837690d5141bc7ed11766bfc1c300f1d7eec284
luping <huangluping@beemwork.com>
2025-09-03 21:59:14 +0800

    引入rpc

--- wisps-user/src/main/test/com/wisps/user/AppTest.java
+++
@@ -1,7 +0,0 @@
-//package com.wisps.user;
-//
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@SpringApplicationConfiguration(classes = UserApplication.class)
-//@WebAppConfiguration
-//public class AppTest {
-//}
